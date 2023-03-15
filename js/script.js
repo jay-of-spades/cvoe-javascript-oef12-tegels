c442965dc0a52ec1a77f99fe5915009e30596e80
Jay Santarina <jay.santarina@gmail.com>
2023-03-15 12:30:32 +0100

    modified html, css, js.

--- js/script.js
+++ js/script.js
@@ -1,122 +1,67 @@
-const select = document.getElementById('maanden');
-const resultaat = document.getElementById('oudenederlandse-namen');
-const orden = [
-  'eerste',
-  'tweede',
-  'derde',
-  'vierde',
-  'vijfde',
-  'zesde',
-  'zevende',
-  'achtste',
-  'negende',
-  'tiende',
-  'elfde',
-  'twaalfde',
-];
-const oudeNamen = [
-  'louwmaand',
-  'sprokkelmaand',
-  'lentemaand',
-  'grasmaand',
-  'bloeimaand',
-  'zomermaand',
-  'hooimaand',
-  'oogstmaand',
-  'herfstmaand',
-  'winmaand',
-  'slachtmaand',
-  'wintermaand',
-];
+const tegelsMaat = document.getElementById('tegels-maat');
 
-const huidigeNamen = [
-  'januari',
-  'februari',
-  'maart',
-  'april',
-  'mei',
-  'juni',
-  'juli',
-  'augustus',
-  'september',
-  'oktober',
-  'november',
-  'december',
-];
 function aantalTegelsBerekenen() {
-  let gekozenMaand = select.value,
-    orde,
-    oude,
-    huidige;
-  switch (gekozenMaand) {
+  let gekozenTegelMaat = tegelsMaat.value;
+  let oppervlakte = oppervlakteBerekenen();
+  let aantalTegels;
+
+  if (isNaN(oppervlakte)) {
+    showError();
+    document.getElementById('hoeveelheid-tegels').innerHTML =
+      'Vul alstublieft de lengte en breedte van uw kamer in.';
+    document.getElementById('hoeveelheid-tegels').classList.remove('hidden');
+    return;
+  }
+
+  switch (gekozenTegelMaat) {
     case '1':
-      orde = orden[0];
-      oude = oudeNamen[0];
-      huidige = huidigeNamen[0];
+      aantalTegels = ((oppervlakte * 10000) / (20 * 20)) * 1.15;
       break;
     case '2':
-      orde = orden[1];
-      oude = oudeNamen[1];
-      huidige = huidigeNamen[1];
+      aantalTegels = ((oppervlakte * 10000) / (30 * 30)) * 1.15;
       break;
     case '3':
-      orde = orden[2];
-      oude = oudeNamen[2];
-      huidige = huidigeNamen[2];
-      break;
-    case '4':
-      orde = orden[3];
-      oude = oudeNamen[3];
-      huidige = huidigeNamen[3];
-      break;
-    case '5':
-      orde = orden[4];
-      oude = oudeNamen[4];
-      huidige = huidigeNamen[4];
-      break;
-    case '6':
-      orde = orden[5];
-      oude = oudeNamen[5];
-      huidige = huidigeNamen[5];
-      break;
-    case '7':
-      orde = orden[6];
-      oude = oudeNamen[6];
-      huidige = huidigeNamen[6];
-      break;
-    case '8':
-      orde = orden[7];
-      oude = oudeNamen[7];
-      huidige = huidigeNamen[7];
-      break;
-    case '9':
-      orde = orden[8];
-      oude = oudeNamen[8];
-      huidige = huidigeNamen[8];
-      break;
-    case '10':
-      orde = orden[9];
-      oude = oudeNamen[9];
-      huidige = huidigeNamen[9];
-      break;
-    case '11':
-      orde = orden[10];
-      oude = oudeNamen[10];
-      huidige = huidigeNamen[10];
-      break;
-    case '12':
-      orde = orden[11];
-      oude = oudeNamen[11];
-      huidige = huidigeNamen[11];
+      aantalTegels = ((oppervlakte * 10000) / (30 * 80)) * 1.15;
       break;
   }
-  resultaat.classList.remove('hidden');
-  resultaat.innerHTML =
-    'De ' +
-    orde +
-    ' maand van het jaar, oftwel ' +
-    huidige +
-    ' noemde men vroeger ook wel de ' +
-    oude +
-    '.';
+
+  aantalTegels = Math.ceil(aantalTegels);
+  console.log(aantalTegels);
+  document.getElementById(
+    'hoeveelheid-tegels'
+  ).innerHTML = `Aantal tegels nodig: ${aantalTegels}`;
+  document.getElementById('hoeveelheid-tegels').classList.remove('hidden');
+}
+
+function oppervlakteBerekenen() {
+  let lengte = document.getElementById('lengte');
+  let breedte = document.getElementById('breedte');
+  let l = parseFloat(lengte.value);
+  let b = parseFloat(breedte.value);
+
+  return l * b;
+}
+
+function showError() {
+  let lengte = document.getElementById('lengte');
+  let breedte = document.getElementById('breedte');
+
+  if (!lengte.value || isNaN(lengte.value)) {
+    lengte.classList.add('input-error');
+  } else {
+    lengte.classList.remove('input-error');
+  }
+
+  if (!breedte.value || isNaN(breedte.value)) {
+    breedte.classList.add('input-error');
+  } else {
+    breedte.classList.remove('input-error');
+  }
+}
+
+document.getElementById('lengte').addEventListener('focus', removeErrorBorder);
+document.getElementById('breedte').addEventListener('focus', removeErrorBorder);
+
+function removeErrorBorder(event) {
+  event.target.classList.remove('input-error');
 }
